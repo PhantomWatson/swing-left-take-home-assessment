952e3745a4746e9155a9acfbbca8c59dfa395afa
Graham Watson <graham@phantomwatson.com>
2025-06-19 18:41:58 -0400

    Set up table sorting with TanStack
    and a bonus styling tweak for the "Deadline" column group

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,90 @@
 "use client";
+
 import React, {useEffect} from "react";
+import {
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  createColumnHelper
+} from '@tanstack/react-table'
 
 export default function Page() {
   const [loading, setLoading] = React.useState(true);
-  const [deadlines, setDeadlines] = React.useState(null);
+  const [deadlines, setDeadlines] = React.useState<Deadline[] | null>(null);
+  const [sorting, setSorting] = React.useState<SortingState>([{id: 'State', desc: false}]);
+
+  type Deadline = {
+    State: string;
+    DeadlineInPerson: string;
+    DeadlineByMail: string;
+    DeadlineOnline: string;
+    ElectionDayRegistration: string;
+    OnlineRegistrationLink: string;
+    Description: string;
+  };
+
+  const columnHelper = createColumnHelper();
+
+  const columns = [
+    {
+      accessorKey: 'State',
+      cell: info => info.getValue().trim() || "Unknown state",
+      header: () => 'State',
+      rowSpan: 2,
+    },
+    columnHelper.group({
+      id: 'Deadline',
+      header: () => 'Deadline',
+      enableSorting: false,
+      columns: [
+        columnHelper.accessor('DeadlineByMail', {
+          header: () => 'By mail',
+          cell: info => formatDate(info.getValue()),
+        }),
+        columnHelper.accessor('DeadlineInPerson', {
+          header: () => 'In person',
+          cell: info => formatDate(info.getValue()),
+        }),
+        columnHelper.accessor('DeadlineOnline', {
+          header: () => 'Online',
+          cell: info => formatDate(info.getValue()),
+        }),
+      ],
+    }),
+    {
+      accessorKey: 'Description',
+      cell: info => info.getValue().trim() || "No description available",
+      header: () => 'Description',
+      rowSpan: 2,
+    },
+    {
+      accessorKey: 'ElectionDayRegistration',
+      cell: info => info.getValue().trim() || "No",
+      header: () => 'Election day registration?',
+      rowSpan: 2,
+    },
+    {
+      accessorKey: 'OnlineRegistrationLink',
+      cell: info => {
+        const link = info.getValue().trim();
+        return link ? linkToNonArchiveDomain(link) : "Link unavailable. Visit the state government homepage for information about registering to vote online.";
+      },
+      header: () => 'Online registration',
+      rowSpan: 2,
+    }
+  ];
+
+  const table = useReactTable({
+    columns,
+    data: deadlines || [],
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    onSortingChange: setSorting,
+    enableSortingRemoval: false,
+    state: {sorting},
+  });
 
   useEffect(() => {
     const load = async () => {
@@ -53,70 +134,81 @@ export default function Page() {
     return date.toLocaleDateString(undefined, { year: 'numeric', month: 'short', day: 'numeric' });
   };
 
-  return (
-    <main>
+  const sortableTable = (
+    <div className="p-2">
+      <div className="h-2" />
       <table className="table table-striped">
         <thead>
-          <tr>
-            <th rowSpan={2}>
-              State
-            </th>
-            <th colSpan={3} style={{textAlign: 'center'}}>
-              Deadline
-            </th>
-            <th rowSpan={2}>
-              Description
-            </th>
-            <th rowSpan={2}>
-              Election day registration?
-            </th>
-            <th rowSpan={2}>
-              Online registration
-            </th>
-          </tr>
-          <tr>
-            <th>
-              By mail
-            </th>
-            <th>
-              In person
-            </th>
-            <th>
-              Online
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {deadlines.map((deadline, index) => (
-            <tr key={index}>
-              <td style={{whiteSpace: 'nowrap'}}>
-                {deadline.State.trim() || "Unknown state"}
-              </td>
-              <td>
-                {formatDate(deadline.DeadlineByMail)}
-              </td>
-              <td>
-                {formatDate(deadline.DeadlineInPerson)}
-              </td>
-              <td>
-                {formatDate(deadline.DeadlineOnline)}
-              </td>
-              <td>
-                {deadline.Description.trim() || "No description available"}
-              </td>
-              <td>
-                {deadline.ElectionDayRegistration.trim() || "No"}
-              </td>
-              <td>
-                {deadline.OnlineRegistrationLink.trim()
-                  ? linkToNonArchiveDomain(deadline.OnlineRegistrationLink.trim())
-                  : "Link unavailable. Visit the state government homepage for information about registering to vote online."
-                }
-              </td>
+          {table.getHeaderGroups().map(headerGroup => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={{
+                      textAlign: header.colSpan > 1 ? 'center' : 'left',
+                      borderTopWidth: header.colSpan > 1 ? '1px' : '0px',
+                      borderLeftWidth: header.colSpan > 1 ? '1px' : '0px',
+                      borderRightWidth: header.colSpan > 1 ? '1px' : '0px',
+                      borderBottomWidth: header.colSpan > 1 ? '0px' : '1px',
+                  }}
+                  >
+                    {header.isPlaceholder ? null : (
+                      <div
+                        style={{
+                          cursor: header.column.getCanSort() ? 'pointer' : 'default',
+                          whiteSpace: 'nowrap',
+                        }}
+                        onClick={header.column.getToggleSortingHandler()}
+                        title={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === 'asc'
+                              ? 'Sort ascending'
+                              : header.column.getNextSortingOrder() === 'desc'
+                                ? 'Sort descending'
+                                : 'Clear sort'
+                            : undefined
+                        }
+                      >
+                        {flexRender(header.column.columnDef.header, header.getContext())}
+                        {{
+                          asc: ' 🔼',
+                          desc: ' 🔽',
+                        }[header.column.getIsSorted() as string] ?? null}
+                      </div>
+                    )}
+                  </th>
+                )
+              })}
             </tr>
           ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map(row => {
+            return (
+              <tr key={row.id}>
+                {row.getVisibleCells().map(cell => {
+                  return (
+                    <td key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
         </tbody>
       </table>
+    </div>
+  );
+
+  return (
+    <main>
+      {sortableTable}
     </main>
   );
 }
