ea5bd47b38cc845ecfeb13e0b8abd3157142698f
Graham Watson <graham@phantomwatson.com>
2025-06-19 17:30:53 -0400

    Add Bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,25 @@
 import React from "react";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <html lang="en">
-      <body>{children}</body>
+    <head>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+              integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+              crossOrigin="anonymous"></Script>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr"
+            crossOrigin="anonymous"/>
+    </head>
+    <body>
+    {children}
+    </body>
     </html>
   );
 }
